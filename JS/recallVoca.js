95b75cb2d3fe7176ba27c768ada9590f984e35a0
Jeonjunhowi <jjh63360@kau.kr>
2022-03-07 11:16:05 +0900

    update: hideCard disappears when slide change event detected

--- JS/recallVoca.js
+++ JS/recallVoca.js
@@ -1,5 +1,3 @@
-
-
 // Make the DIV element draggable:
 let isDrawing = false;
 let x = 0; 
@@ -8,71 +6,70 @@ let firstX = 0;
 let firstY=0;
 var hideCardDiv;
 
-function setHideCardDiv(i){
-  hideCardDiv = document.getElementById("hideCard" + i);
-  const defaultHideCardHeight = hideCardDiv.clientHeight;
+// function setHideCardDiv(i){
+//   hideCardDiv = document.getElementById("hideCard" + i);
+//   const defaultHideCardHeight = hideCardDiv.clientHeight;
 
-  console.log("기본 hideCard 길이: " + defaultHideCardHeight)
-  console.log("기본 hideCard 높이: " + hideCardDiv.clientTop)
-
-  hideCardDiv.addEventListener('click', e => {
-    console.log("clicked")
-   })
+//   // console.log("기본 hideCard 길이: " + defaultHideCardHeight)
+//   // console.log("기본 hideCard 높이: " + hideCardDiv.clientTop)
    
-   hideCardDiv.addEventListener('pointerdown', e=> {
-     firstY = e.clientY;
+//    hideCardDiv.addEventListener('pointermove', e => {
+//      if (isDrawing == true){
+//        x = e.clientX;
+//        y = e.clientY;
    
-     console.log("firstY: " + firstY);
-     isDrawing = true;
-   })
-   
-   hideCardDiv.addEventListener('pointermove', e => {
-     if (isDrawing == true){
-       x = e.clientX;
-       y = e.clientY;
-   
-       //console.log("x: " + x + "y: " + y)
+//        //console.log("x: " + x + "y: " + y)
        
-       deltaY = firstY-y;
-       //style 현황 가져오기
-       var hideCardDivStyle = hideCardDiv.currentStyle || window.getComputedStyle(hideCardDiv);
-       hideCardDiv.style.marginTop = -deltaY*2 + "px";
+//        deltaY = firstY-y;
+//        //style 현황 가져오기
+//        var hideCardDivStyle = hideCardDiv.currentStyle || window.getComputedStyle(hideCardDiv);
+//        hideCardDiv.style.marginTop = -deltaY*2 + "px";
        
-       //console.log("deltaY: ", deltaY);
-       //console.log("hideCardDiv.style.marginTop: " +  hideCardDivStyle.clientMarginTop);
-       //console.log("hideCardDiv.clientHeight: " + hideCardDivStyle.marginTop);
-       if(deltaY < -defaultHideCardHeight + 30) unvisibleHideCard();
-     }
-   })
+//        //console.log("deltaY: ", deltaY);
+//        //console.log("hideCardDiv.style.marginTop: " +  hideCardDivStyle.clientMarginTop);
+//        //console.log("hideCardDiv.clientHeight: " + hideCardDivStyle.marginTop);
+//        if(deltaY < -defaultHideCardHeight + 30) unvisibleHideCard();
+//      }
+//    })
    
-   window.addEventListener('pointerup', e => {
-     isDrawing = false;
-     
-     hideCardDiv.style.height = defaultHideCardHeight+"px";
-     hideCardDiv.style.marginTop = 0;
-   })
+//    //스페이스바 누르면 visibility 바꾸기
+//    document.body.onkeyup = function(e){
+//      //스페이스바는 32번 키
+//      if(e.keyCode == 32){
+//        unvisibleHideCard()
+//      }
+//    }
    
-   //스페이스바 누르면 visibility 바꾸기
-   document.body.onkeyup = function(e){
-     //스페이스바는 32번 키
-     if(e.keyCode == 32){
-       unvisibleHideCard()
-     }
-   }
-   
+   /**
+    * 카드 숨기기
+    */
    function unvisibleHideCard(){
      hideCardDiv.style.visibility = 'hidden';
    }
+
+//   /**
+//    * 슬라이드 index받아오기
+//    * @param {int} index 
+//    */
+//   function slideIndexCount(index){
+//     alert(index+'번째 slide입니다.');
+//   }
+
+  /**
+   * 슬라이드 넘기면 카드 아래로 숨기기
+   * @param {int} slideIndex
+   */
+  function slideCardHide(slideIndex){
+    var hideCardDiv = document.getElementById("hideCard" + slideIndex);
+
+    // alert(slideIndex+'번째 slide입니다.');
+    const defaultHideCardHeight = hideCardDiv.clientHeight;
+
+    hideCardDiv.style.transform = "translate("+ 0 +"px," + 400 +"px)";
+
+    hideCardDiv.ontransitionend = () => {
+      hideCardDiv.style.opacity = 0;
+    };
+    
+    
   }
-
-  function slideIndexCount(index){
-    alert(index+'번째 slide입니다.');
-  }
-
-
-
-
-
-
-
-
