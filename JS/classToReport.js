b13b09305255ac9b80ac4678e029179d5dc837a2
Jeonjunhowi <jjh63360@kau.kr>
2022-03-08 13:06:57 +0900

    update: copy folder for github Pages

---
+++ JS/classToReport.js
@@ -0,0 +1,48 @@
+/**
+ * 세트 리포트 정보 출력하기
+ */
+ const sampleReportInfo=
+ {
+     "classCode": "1111",
+     "setInfo":[
+         {"setCode": 0001, "setType": "word", "setName": "DA English 1 Sample"},
+         {"setCode": 0002, "setType": "word", "setName": "주니어 능률보카 기본 - DAY 01"},
+         {"setCode": 0003, "setType": "word", "setName": "(샘플) EBS 초등 5학년 영어단어"}
+     ]
+ }
+ 
+ for(idx in sampleReportInfo.setInfo)
+ {
+     $(".reportUnorderedList").append("<li class = \"reportNameList\"><div id=\"vocaType\"><img src=\"/FrontWeb/Images/classCardVoca.png\" alt=\"단어\"></div><p style = \"margin-left : 10px;\">" + sampleReportInfo.setInfo[idx].setName +"</p></li>")
+ }
+ 
+//  const reportTable = document.getElementById(reportTable);
+
+//  const newRow = reportTable.insertRow(0);
+
+//  const newCell1 = newRow.insertCell(0);
+//  const newCell2 = newRow.insertCell(1);
+//  const newCell3 = newRow.insertCell(2);
+//  const newCell4 = newRow.insertCell(3);
+
+ const sampleStudentReport = 
+ {
+   "setCode" : 0001,
+   "studentInfo":[
+     {"name" : "전준휘" , "id":"jjh63360", "speaking":10, "remember":20, "recall":30, "spell" : 40, "repeat":50, "test":100},
+     {"name" : "정세은" , "id":"tpdms0159", "speaking":10, "remember":20, "recall":30, "spell" : 40, "repeat":60, "test":100},
+     {"name" : "이도훈" , "id":"bigeyec", "speaking":10, "remember":20, "recall":30, "spell" : 40, "repeat":70, "test":100}
+   ]
+ }
+
+ 
+  //  newCell1.innerText = sampleStudentReport[0].studentInfo.name;
+  //  newCell2.innerText = sampleStudentReport[0].studentInfo.id;
+  //  newCell3.innerText = sampleStudentReport[0].studentInfo.id;
+  //  newCell4.innerText = sampleStudentReport[0].studentInfo.id;
+
+  for (idx in sampleStudentReport.studentInfo) {
+    $("#reportTable").append("<tr><td v-if=\"studentInfo\" rowspan = \"2\" id =\"reportTd\"><img src = \"/FrontWeb/Images/default_photo.png\" style = \"border-radius : 50px; \" id=\"reportImg\"></td><td class = \"studentName\" id = \"student\" style=\"border: solid 1px rgb(230, 230, 230); border-width: 1px 1px 0 0; \">" + sampleStudentReport.studentInfo[idx].name + "</td><td v-if=\"studentInfo\" id=\"reportTd\">" + sampleStudentReport.studentInfo[idx].speaking + "</td><td id=\"reportTd\">" + sampleStudentReport.studentInfo[idx].remember + "</td><td v-if=\"studentInfo\" id=\"reportTd\">" + sampleStudentReport.studentInfo[idx].recall + "</td><td v-if=\"studentInfo\" id=\"reportTd\">" + sampleStudentReport.studentInfo[idx].spell + "</td><td id=\"reportTd\">" + sampleStudentReport.studentInfo[idx].repeat + "</td><td v-if=\"studentInfo\" id=\"reportTd\">"  + sampleStudentReport.studentInfo[idx].test + "</td></tr>");
+    $("#reportTable").append("<tr><td v-if=\"studentInfo\" id = \"student\">" + sampleStudentReport.studentInfo[idx].id + "</td><td id=\"reportTd\">N</td><td id=\"reportTd\">N</td><td id=\"reportTd\">N</td><td id=\"reportTd\">N</td><td id=\"reportTd\">N</td><td id=\"reportTd\">N</td></tr>")
+  }
+
---
+++ JS/rememberVocaJquery.js
@@ -0,0 +1,55 @@
+/**
+ * JQuery로 html 붙이기
+ * 
+ */
+
+ const sampleVoca =
+ {
+   "setCode": 1234,
+   "setName": "DA_ENGLISH_A반 Sample Set",
+   "voca": [
+     { "vocaindex": 1, "englishVoca": "tree", "koreanMean": "나무" },
+     { "vocaindex": 2, "englishVoca": "airpod", "koreanMean": "에어팟" },
+     { "vocaindex": 3, "englishVoca": "camera", "koreanMean": "카메라" },
+     { "vocaindex": 4, "englishVoca": "arduino", "koreanMean": "아두이노" },
+     { "vocaindex": 5, "englishVoca": "banana", "koreanMean": "바나나" },
+     { "vocaindex": 6, "englishVoca": "bread", "koreanMean": "빵" },
+     { "vocaindex": 7, "englishVoca": "bowl", "koreanMean": "공기/밥그릇" },
+     { "vocaindex": 8, "englishVoca": "butter", "koreanMean": "버터" },
+     { "vocaindex": 9, "englishVoca": "cake", "koreanMean": "케잌" },
+     { "vocaindex": 10, "englishVoca": "chicken", "koreanMean": "닭" },
+     { "vocaindex": 11, "englishVoca": "actually", "koreanMean": "사실은" },
+     { "vocaindex": 12, "englishVoca": "again", "koreanMean": "다시" },
+     { "vocaindex": 13, "englishVoca": "already", "koreanMean": "벌써" },
+     { "vocaindex": 14, "englishVoca": "alright", "koreanMean": "괜찮다" },
+     { "vocaindex": 15, "englishVoca": "also", "koreanMean": "또한" },
+     { "vocaindex": 16, "englishVoca": "always", "koreanMean": "항상" },
+     { "vocaindex": 17, "englishVoca": "and", "koreanMean": "그리고" },
+     { "vocaindex": 18, "englishVoca": "angry", "koreanMean": "화가 나다" },
+     { "vocaindex": 19, "englishVoca": "animal", "koreanMean": "동물" },
+     { "vocaindex": 20, "englishVoca": "another", "koreanMean": "또 하나/다른" }
+   ]
+ }
+
+ for (var i in sampleVoca.voca) {
+  
+   var html = '';
+   
+   html += '<div class=\"swiper-slide\">'
+   html += '<div> <div class=\"topVocaCard\"> <p style=\"color: greenyellow;\"> v0 | </p>'
+   html += ' <p style=\"color: white;\"> 10 </p> </div> <div class=\"middleVocaCard\">'
+   html += '<div id=\"topCard\"> <img src=\"/FrontWeb/Images/volume.png\" id=\"topCardImg\">'
+   html += '<img src=\"/FrontWeb/Images/star.png\" id=\"topCardImg\"> </div> <div class=\"middleCard\" id=\"vocaEnglish\">'
+   html += '<p id=\"middleVoca\">'
+   html += sampleVoca.voca[i].englishVoca
+   html += '</p> </div> <div class=\"middleCard\" id=\"vocaKorean\" style=\"border: none;\">'
+   html += '<div class=\"middelCard\ hideCard" id = \"hideCard' + i + '\" onmouseover="setHideCardDiv('+ i +')"></div> <p id=\"middleVoca\">'
+   html += sampleVoca.voca[i].koreanMean
+   html += '</p></div>'
+   html += '</div > <div class=\"bottomVocaCard\"><div id=\"bottomButton\">'
+   html += '<button id=\"buttonStyle\" style=\"background-color: rgb(149, 233, 24); color: white;\">이제 알아요</button>'
+
+   html += '<p id = \"keyBoard\"> ENTER</p></div><div id=\"bottomButton\"><button id=\"buttonStyle\">나중에 한번 더</button>'
+   html += '<p id=\"keyBoard\">SPACE</p> </div> </div> <div id="blankDivToHide"></div> </div></div>'
+   $(".swiper-wrapper").append(html);
+ }
