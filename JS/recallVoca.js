5738b0c1d6d2e984518a16358743bea25f56ddca
Jeonjunhowi <jjh63360@kau.kr>
2022-03-08 09:38:54 +0900

    add: tts module

--- JS/recallVoca.js
+++ JS/recallVoca.js
@@ -19,6 +19,8 @@ function unvisibleHideCard(){
  */
 function slideCardHide(slideIndex){
   var hideCardDiv = document.getElementById("hideCard" + slideIndex);
+  console.log(englishAnswerSetObject.vocaAnswerSet[slideIndex].englishVoca)
+  speech(englishAnswerSetObject.vocaAnswerSet[slideIndex].englishVoca)
 
   // alert(slideIndex+'번째 slide입니다.');
   const defaultHideCardHeight = hideCardDiv.clientHeight;
@@ -30,6 +32,11 @@ function slideCardHide(slideIndex){
   };
 }
 
+/**
+ * index번째 문의 option을 클릭하였을 때 정답 여부를 확인시켜주는 alert를 추가합니다.
+ * @param {int} index 
+ * @param {int} option 
+ */
 function studentOptionClicked(index, option){
   //alert(index + "번째 문제의 " + option + "번째 option을 선택!")
   if(englishAnswerSetObject.vocaAnswerSet[index].koreanMeanOptionNum == option){
@@ -39,3 +46,45 @@ function studentOptionClicked(index, option){
     alert("오답입니다!");
   }
 }
+
+  
+  
+var voices = [];
+function setVoiceList() {
+  voices = window.speechSynthesis.getVoices();
+}
+setVoiceList();
+
+if (window.speechSynthesis.onvoiceschanged !== undefined) {
+  window.speechSynthesis.onvoiceschanged = setVoiceList;
+}
+function speech(txt) {
+    if(!window.speechSynthesis) {
+      alert("음성 재생을 지원하지 않는 브라우저입니다. 크롬, 파이어폭스 등의 최신 브라우저를 이용하세요");
+    return;
+  }
+  var lang = 'ko-KR';
+  var utterThis = new SpeechSynthesisUtterance(txt);
+    utterThis.onend = function (event) {
+    console.log('end');
+  };
+  utterThis.onerror = function(event) {
+    console.log('error', event);
+  };
+  var voiceFound = false;
+
+  for(var i = 0; i < voices.length ; i++) {
+    if(voices[i].lang.indexOf(lang) >= 0 || voices[i].lang.indexOf(lang.replace('-', '_')) >= 0) {
+      utterThis.voice = voices[i];
+      voiceFound = true;
+    }
+  }
+  
+  utterThis.lang = lang;
+  utterThis.pitch = 1;
+  utterThis.rate = 1; //속도
+  window.speechSynthesis.speak(utterThis);
+}
+
+  
+
